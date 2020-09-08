1b6d6291828ae49b6b287bf5197143d30f56aada
ZitongLu1996 <zitonglu1996@gmail.com>
2020-09-08 16:17:19 +0800

    Version 1.1.3.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='neurora',
-    version='1.1.3.5',
+    version='1.1.3.6',
     description=(
         'A Python Toolbox for Multimode Neural Data Representation Analysis'
     ),
